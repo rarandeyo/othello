617c1e784feefc5e8e6d6f3fbd9a66aef30e50e1
1F10230139 <s1F102301392@iniad.org>
2023-05-11 19:42:18 +0900

    add new file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,59 +1,73 @@
+import { useState } from 'react';
 import styles from './index.module.css';
-
 const Home = () => {
+  const [turnColor, setTurncolor] = useState(1);
+  // prettier-ignore
+  const[board,setboard]=useState([
+    [0,0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,0,],
+    [0,0,0,1,2,0,0,0,],
+    [0,0,0,2,1,0,0,0,],
+    [0,0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,0,],
+  ])
+  const clickLine = (x: number, y: number) => {
+    console.log(x, y);
+    const newborad: number[][] = JSON.parse(JSON.stringify(board));
+    if (board[y + 1] !== undefined && board[y + 1][x] !== 0 && board[y + 1][x] !== turnColor) {
+      for (let i = y + 1; i < board.length; i++) {
+        if (board[i][x] === turnColor) {
+          newborad[y][x] = turnColor;
+          for (let j = y + 1; j < i; j++) {
+            newborad[j][x] = turnColor;
+          }
+          setTurncolor(3 - turnColor);
+        }
+      }
+    }
+    if (board[y - 1] !== undefined && board[y - 1][x] !== 0 && board[y - 1][x] !== turnColor) {
+      for (let i = y - 1; i < board.length && i >= 0; i = i - 1) {
+        if (board[i][x] === turnColor) {
+          newborad[y][x] = turnColor;
+          for (let j = y - 1; j > i; j = j - 1) {
+            newborad[j][x] = turnColor;
+          }
+          setTurncolor(3 - turnColor);
+        }
+      }
+    }
+
+    if (board[y][x - 1] !== undefined && board[y][x - 1] !== 0 && board[y][x - 1] !== turnColor) {
+      newborad[y][x] = turnColor;
+      setTurncolor(3 - turnColor);
+    }
+    if (board[y][x + 1] !== undefined && board[y][x + 1] !== 0 && board[y][x + 1] !== turnColor) {
+      newborad[y][x] = turnColor;
+      setTurncolor(3 - turnColor);
+    }
+
+    setboard(newborad);
+  };
+
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code} style={{ backgroundColor: '#fafafa' }}>
-            pages/index.js
-          </code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a className={styles.card} href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <div className={styles.square}>
+        {board.map((raw, y) =>
+          raw.map((color, x) => (
+            <div className={styles.line} key={`${x}-${y}`} onClick={() => clickLine(x, y)}>
+              {color !== 0 && (
+                <div
+                  className={styles.stone}
+                  style={{ background: color === 1 ? '#000' : '#fff' }}
+                />
+              )}
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 };
-
 export default Home;
