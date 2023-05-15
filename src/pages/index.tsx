c241bc8ad586496646d72dda5e59973ee9ca3405
1F10230139 <s1F102301392@iniad.org>
2023-05-15 21:10:31 +0900

    add new file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,53 +15,51 @@ const Home = () => {
   ])
   const clickLine = (x: number, y: number) => {
     console.log(x, y);
-    const newborad: number[][] = JSON.parse(JSON.stringify(board));
-    if (board[y + 1] !== undefined && board[y + 1][x] !== 0 && board[y + 1][x] !== turnColor) {
-      for (let i = y + 1; i < board.length; i++) {
-        if (board[i][x] === turnColor) {
-          newborad[y][x] = turnColor;
-          for (let j = y + 1; j < i; j++) {
-            newborad[j][x] = turnColor;
-          }
-          setTurncolor(3 - turnColor);
+    const newBoard: number[][] = JSON.parse(JSON.stringify(board));
+    const directions = [
+      { dx: 0, dy: -1 }, // 上
+      { dx: 0, dy: 1 }, // 下
+      { dx: -1, dy: 0 }, // 左
+      { dx: 1, dy: 0 }, // 右
+      { dx: -1, dy: -1 }, // 左上
+      { dx: 1, dy: -1 }, // 右上
+      { dx: -1, dy: 1 }, // 左下
+      { dx: 1, dy: 1 }, // 右下
+    ];
+
+    for (const { dx, dy } of directions) {
+      const nx = x + dx;
+      const ny = y + dy;
+
+      if (newBoard[ny] !== undefined && newBoard[ny][nx] !== 0 && newBoard[ny][nx] !== turnColor) {
+        let i = ny;
+        let j = nx;
+
+        while (
+          i >= 0 &&
+          i < board.length &&
+          j >= 0 &&
+          j < board.length &&
+          board[i][j] !== turnColor
+        ) {
+          i += dy;
+          j += dx;
         }
-      }
-    }
-    if (board[y - 1] !== undefined && board[y - 1][x] !== 0 && board[y - 1][x] !== turnColor) {
-      for (let i = y - 1; i < board.length && i >= 0; i = i - 1) {
-        if (board[i][x] === turnColor) {
-          newborad[y][x] = turnColor;
-          for (let j = y - 1; j > i; j = j - 1) {
-            newborad[j][x] = turnColor;
-          }
-          setTurncolor(3 - turnColor);
-        }
-      }
-    }
-    if (board[y][x - 1] !== undefined && board[y][x - 1] !== 0 && board[y][x - 1] !== turnColor) {
-      for (let i = x - 1; i >= 0; i = i - 1) {
-        if (board[y][i] === turnColor) {
-          newborad[y][x] = turnColor;
-          for (let j = x - 1; j > i; j = j - 1) {
-            newborad[y][j] = turnColor;
-          }
-          setTurncolor(3 - turnColor);
-        }
-      }
-    }
-    if (board[y][x + 1] !== undefined && board[y][x + 1] !== 0 && board[y][x + 1] !== turnColor) {
-      for (let i = x + 1; i < board.length; i++) {
-        if (board[y][i] === turnColor) {
-          newborad[y][x] = turnColor;
-          for (let j = x + 1; j < i; j++) {
-            newborad[y][j] = turnColor;
+
+        if (i >= 0 && i < board.length && j >= 0 && j < board.length && board[i][j] === turnColor) {
+          for (
+            let k = y, l = x;
+            (dy > 0 ? k <= i : k >= i) && (dx > 0 ? l <= j : l >= j);
+            k += dy, l += dx
+          ) {
+            newBoard[k][l] = turnColor;
           }
           setTurncolor(3 - turnColor);
         }
       }
     }
 
-    setboard(newborad);
+    setboard(newBoard);
   };
 
   return (
